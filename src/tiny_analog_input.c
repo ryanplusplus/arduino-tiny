9bd32f372bd23560db6b4bdeb67bff8f42b72814
Ryan Hartlage <ryanplusplus@gmail.com>
2020-12-24 08:47:11 -0500

    Fixed analog range

--- src/tiny_analog_input.c
+++ src/tiny_analog_input.c
@@ -10,7 +10,7 @@
 static tiny_analog_input_counts_t read(i_tiny_analog_input_t* _self)
 {
   reinterpret(self, _self, tiny_analog_input_t*);
-  return analogRead(self->_private.pin) << 8;
+  return analogRead(self->_private.pin) << 6;
 }
 
 static const i_tiny_analog_input_api_t api = { read };
